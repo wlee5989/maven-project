df4c4694f3f90debd1b23f927ae1e902378d3235
WL5989 <WL5989@NABWCHLTIS001.EU.BIDBW.COM>
2017-02-10 15:36:40 -0500

    added comment

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
+  //TODO: greet method
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
